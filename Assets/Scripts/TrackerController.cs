8905bc58e61ad38091e586b6e84ad74224f43aed
Repin <s-repinn@bsd405.org>
2023-09-21 13:25:33 -0700

    Added enum for ai types

---
+++ Assets/Scripts/TrackerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float offset;
+
+    [System.Flags]
+    public enum AI 
+    {
+        Melee = 1,
+        Range = 2,
+    }
+    [SerializeField] AI ai = AI.Melee;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ai == AI.Melee) 
+        {
+            transform.position = target.transform.position;
+        }
+        else if (ai == AI.Range) 
+        {
+            transform.position = target.transform.position;
+            var dir = parent.transform.position - Camera.main.WorldToScreenPoint(transform.position);
+            var angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(-angle + 90, Vector3.forward);
+            transform.position += dir.normalized * offset;
+        }
+    }
+}
