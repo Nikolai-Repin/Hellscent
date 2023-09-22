2eaefb2c8e945def878e8e291acea079a88d1305
Nieman <s-niemanj@bsd405.org>
2023-09-22 12:20:57 -0700

    lifetime test

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    public int maxLife;
+    private int lifeTime = 0;
+
+    void Update() {
+        //lifeTime++;
+        if (lifeTime >= maxLife) {
+            Destroy(gameObject);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         Destroy(gameObject);
