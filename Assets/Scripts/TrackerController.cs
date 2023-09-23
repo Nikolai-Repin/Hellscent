86e44cbd27c944d0f794b92aa63681019ef60e96
nikolai <xylempalace@icloud.com>
2023-09-22 18:09:03 -0700

    Completed basic enemy ai

--- Assets/Scripts/TrackerController.cs
+++ Assets/Scripts/TrackerController.cs
@@ -1,22 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class TrackerController : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    [Header("Base Settings")]
     [SerializeField] private float offset;
-
+    [SerializeField] private LayerMask walls;
+    [SerializeField] private LayerMask enemies;
     [System.Flags]
-    public enum AI 
+    public enum AI
     {
         Melee = 1,
         Range = 2,
     }
     [SerializeField] AI ai = AI.Melee;
 
-    // Update is called once per frame
-    void Update()
+    [Space, Header("Pathfinder Settings")]
+    [SerializeField] private float endReachedDistanceMelee;
+    [SerializeField] private float endReachedDistanceRange;
+
+    private Transform target;
+    private AIPath aiPath;
+
+    private void Start()
+    {
+        target = GameObject.FindWithTag("player").transform;
+        aiPath = transform.parent.GetComponent<AIPath>();
+        if (ai == AI.Melee)
+        {
+            aiPath.endReachedDistance = endReachedDistanceMelee;
+        }
+        else if (ai == AI.Range)
+        {
+            aiPath.endReachedDistance = endReachedDistanceRange;
+        }
+    }
+
+    private void Update()
     {
         if (ai == AI.Melee) 
         {
@@ -29,6 +51,27 @@ public class TrackerController : MonoBehaviour
             var angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(-angle + 90, Vector3.forward);
             transform.position += dir.normalized * offset;
+
+            RaycastHit2D hit = Physics2D.Raycast(target.position, dir.normalized, offset, walls.value);
+            if (hit.collider != null)
+            {
+                
+                hit = Physics2D.Raycast(target.position, dir.normalized, offset, enemies.value | walls.value);
+                if (hit.transform.tag == "Enemy")
+                {
+                    Debug.DrawLine(target.position, transform.position, Color.blue);
+                }
+                else
+                {
+                    Debug.DrawLine(target.position, transform.position, Color.red);
+                    transform.position = target.transform.position;
+                }
+                
+            }
+            else
+            {
+                Debug.DrawLine(target.position, transform.position, Color.green);
+            }
         }
     }
 }
