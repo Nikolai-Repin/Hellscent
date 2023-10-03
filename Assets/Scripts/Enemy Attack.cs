a6fe0f4316292964eaff9e78ee2c692c5d9a336a
Knotwell <s-knotwella@bsd405.org>
2023-10-03 12:50:22 -0700

    Added weapon as child of Enemy prefab and have it follow enemy arround git push
    w

--- Assets/Scripts/Enemy Attack.cs
+++ Assets/Scripts/Enemy Attack.cs
@@ -3,22 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
+
+
+
 {
+    private float cooldown;
+    private GameObject parent;
+    private SpriteRenderer sr;
+    private Controller controller;
+    public int NextAttackTimer = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        parent = transform.parent.gameObject;
+        sr = GetComponent<SpriteRenderer>();
+        controller = parent.GetComponent<Controller>();
+        controller.heldWeapons.Add(GetComponent<Weapon>());
+        controller.ChangeWeapon(controller.heldWeapons.Count-1);
     }
 
     // Update is called once per frame
     void Update()
     {
+      transform.position = parent.transform.position;
+       if(NextAttackTimer > 5){
         Attacking();
+       NextAttackTimer = -1;
+       }
+       NextAttackTimer++;
     }
-function Attacking(){
-
-var Shoot = Instantiate(bullet)
-
+public void Attacking(){
 
 
 
