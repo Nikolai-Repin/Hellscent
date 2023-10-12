c850ee979d510d9de0e58ee149d0a91169e3940c
Knotwell <s-knotwella@bsd405.org>
2023-10-12 13:09:43 -0700

    Bug fixes

--- Assets/Scripts/Enemy Weapon.cs
+++ Assets/Scripts/Enemy Weapon.cs
@@ -8,7 +8,8 @@ public class EnemyWeapon : Weapon
 
 {
     // cooldownTime is set to fire once per second
-    protected GameObject target;
+    public GameObject target;
+    protected SpriteRenderer sprn;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,14 +24,24 @@ public class EnemyWeapon : Weapon
     {
         Vector3 temp = new Vector3();
         temp = parent.transform.position;
-        temp.x += -.5F;
+        temp.x += -1.7F; //Puts weapon on edge of player in Sample Scene; 
         transform.position = temp;
         Vector3 Look = transform.InverseTransformPoint(target.transform.position);
+        var dir = transform.InverseTransformPoint(target.transform.position);
         float Angle = Mathf.Atan2(Look.y, Look.x) * Mathf.Rad2Deg;
         transform.Rotate(0,0, Angle);
+         var angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+        /*if (angle < 0)
+        {
+            sprn.flipY = true;
+        } else {
+            sprn.flipY = false;
+        }*/
+
         Fire();
+         Debug.Log("Finished Script");
         cooldown -= Time.deltaTime;
-        Debug.Log(cooldown);
+        
     }
 }
 
--- Assets/Scripts/FacePlayer.cs
+++ Assets/Scripts/FacePlayer.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FacePlayer : Weapon
+public class FacePlayer : MonoBehaviour
 {
+    public GameObject target2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Finished Script");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 Look = transform.InverseTransformPoint(target.transform.position);
-        float Angle = Mathf.Atan2(Look.y, Look.x) * Mathf.Rad2Deg;
-        transform.Rotate(0,0, Angle);
+        
+        Vector3 playerPos = target2.transform.position;
+        Vector3 enemy = gameObject.transform.position;
+        Vector3 delta = new Vector3(playerPos.x - enemy.x, 0.0f, playerPos.z - enemy.z);
+        Quaternion rotation = Quaternion.LookRotation(delta);
+        
+      gameObject.transform.rotation = rotation;
+        Debug.Log("Finished Script");
     }
 }
