c2d69c854edc1e6b316b705b2a011ac3e2e95cb0
nikolai <xylempalace@icloud.com>
2023-10-11 19:17:01 -0700

    added comments to prefabPlacement script

--- Assets/Scripts/prefabPlacement.cs
+++ Assets/Scripts/prefabPlacement.cs
@@ -23,21 +23,25 @@ public class prefabPlacement : MonoBehaviour
 	List<string> directionNumber = new List<string>(){"North", "West", "South", "East"};
 
 	void Start () {
+		// Creates the starting room and starts the generation
 		GameObject startRoom = createRoom(spawnRoom, null, false);
 		createDungeon(startRoom, 4, 5);
 	}
 
 	private void createDungeon(GameObject startRoom, int mainBranchLen, int branchCap)
     {
+		// Loops between all available doors of the current room
 		for (int i = 0; i < getNumAvailable(startRoom.GetComponent<RoomInfo>()); i++)
         {
 			if (mainBranchLen > 0)
-            {	
+            {
+				// Determines if the room being created is a main branch(a branch that leads to the end)
 				createDungeon(createRoom(rooms[0], startRoom), mainBranchLen - 1, branchCap - 1);
 				mainBranchLen = 0;
             }
 			else if (branchCap > 0 && Random.Range(0, 3) == 0)
             {
+				// Determines if any more branches should be added and rolls a dice to see if the room will be created
 				createDungeon(createRoom(rooms[0], startRoom), 0, branchCap - 1);
 			}
         }
@@ -45,32 +49,37 @@ public class prefabPlacement : MonoBehaviour
 
 	private GameObject createRoom(GameObject room, GameObject toAlign, bool start=false)
     {
-		RoomInfo data = room.GetComponent<RoomInfo>();
+		// Initializes a room
 		float randomDir = start ? Random.Range(0, 4) : 0;
 		GameObject createdRoom = Instantiate(room, new Vector2(0, 0), Quaternion.Euler(0, 0, randomDir * 90));
 		createdRoom.transform.SetParent(transform, false);
 		dungeon.Add(createdRoom);
 		if (!start)
         {
+			// Aligns created room to previous room
 			alignRooms(toAlign.transform, createdRoom.transform, roomSpacing);
 		}
 		return createdRoom;
 	}
 
 	private void alignRooms(Transform origin, Transform created, float spacing) {
+		// Gets all necessary information about the origin room
 		RoomInfo origin_data = origin.GetComponent<RoomInfo>();
 		string direction_origin = origin_data.doorDirection[findAvailableDoor(origin_data)];
 		Transform origin_join_point = origin.Find("Join Point " + direction_origin).Find("Join Point");
 		origin_data.doorOccupation[origin_data.doorDirection.IndexOf(direction_origin)] = true;
 
+		// Finds a door on created room that will connect with origin
 		string true_dir = directionNumber[directionNumber.IndexOf(direction_origin) + (int) (origin.rotation.eulerAngles.z / 90)];
 		string direction_created;
 		RoomInfo created_data = created.GetComponent<RoomInfo>();
 		string true_created_dir;
 		if (created_data.doorDirection.Count == 4) {
+			// If the room has four doors do not rotate
 			direction_created = reverseDirection[true_dir];
 			true_created_dir = direction_created;
 		} else {
+			// Randomly rotates rooms with less than 4 doors
 			created.transform.rotation = Quaternion.Euler(0, 0, 0);
 			string selected_door = created_data.doorDirection[Random.Range(0, created_data.doorDirection.Count)];
 			int selected_door_dir = directionNumber.IndexOf(selected_door);
@@ -82,6 +91,7 @@ public class prefabPlacement : MonoBehaviour
 		}
 		Transform created_join_point = created.Find("Join Point " + direction_created).Find("Join Point");
 
+		// Aligns the created room to the origin room
 		Vector2 shift;
 		if (true_created_dir == "North" || true_created_dir == "South") {
 			shift = new Vector2(origin_join_point.position.x - created_join_point.position.x, origin_join_point.position.y + ((true_created_dir == "North" ? -spacing : roomSpacing)) - created_join_point.position.y);
@@ -93,6 +103,7 @@ public class prefabPlacement : MonoBehaviour
 
 	private int findAvailableDoor(RoomInfo data)
     {
+		// Finds a random available door for a given room
 		List<int> available = new List<int>();
 		for (int i = 0; i < data.doorOccupation.Count; i++)
         {
@@ -106,6 +117,7 @@ public class prefabPlacement : MonoBehaviour
 
 	private int getNumAvailable(RoomInfo data)
     {
+		// Gets the number of all available doors for a given room
 		int count = 0;
 		foreach (bool occupied in data.doorOccupation)
         {
