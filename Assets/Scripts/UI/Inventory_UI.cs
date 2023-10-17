a753d26f2258f7c5bd98e42a8635ea59db4361df
Wu <s-wube@bsd405.org>
2023-10-17 13:11:14 -0700

    Added Inventory UI

---
+++ Assets/Scripts/UI/Inventory_UI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_UI : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+
+    public List<Slot_UI> slots = new List<Slot_UI>();
+
+    void Update() {
+        // Press TAB to open Inventory
+        if(Input.GetKeyDown(KeyCode.Tab)) {
+            ToggleInventory();
+        }
+    }
+
+    public void ToggleInventory() {
+        // Checks if Inventory is opened or not
+        if(!inventoryPanel.activeSelf) {
+            inventoryPanel.SetActive(true);
+            // Setup();
+        }
+        else {
+            inventoryPanel.SetActive(false);
+        }
+    }
+
+    /* void Setup() {
+        // Loops through all the slots in the inventory
+        if(slots.Count == player.inventory.slots.Count) {
+            for(int i = 0; i < slots.Count; i++) {
+                // Checks to see if there is an item in the slot
+                if(player.inventory.slots[i].type != CollectableType.NONE) {
+
+                }
+            }
+        }
+    } */
+}
