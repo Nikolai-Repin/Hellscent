db5a2f4a7a47c410c6e16b48ccff0a254abfb418
Alex Knotwell <s-knotwella@bsd405.org>
2023-10-24 12:57:29 -0700

    Added Input System Package + Created Folder with Slime Animations

---
+++ Assets/Prefabs/Angry Slime/MovementAnimation.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementAnimation : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    bool canMove;
+    Vector2 movementInput;
+
+    SpriteRenderer spriteRenderer;
+
+    Rigidbody2D rb;
+
+    Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          rb = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+        private void FixedUpdate() {
+
+        if(canMove) {
+
+            // If movement input is not 0, try to move
+
+            if(movementInput != Vector2.zero){
+
+               
+
+                bool success = TryMove(movementInput);
+
+ 
+
+                if(!success) {
+
+                    success = TryMove(new Vector2(movementInput.x, 0));
+
+                }
+
+ 
+
+                if(!success) {
+
+                    success = TryMove(new Vector2(0, movementInput.y));
+
+                }
+
+               
+
+                animator.SetBool("isMoving", success);
+
+            } else {
+
+                animator.SetBool("isMoving", false);
+
+            }
+
+ 
+
+            // Set direction of sprite to movement direction
+
+            if(movementInput.x < 0) {
+
+                spriteRenderer.flipX = true;
+
+            } else if (movementInput.x > 0) {
+
+                spriteRenderer.flipX = false;
+
+            }
+
+        }
+
+    }
+
+ 
+
+    private bool TryMove(Vector2 direction) {
+
+        if(direction != Vector2.zero) {
+
+            // Check for potential collisions
+
+            int count = rb.Cast(
+
+                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+
+                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
+
+ 
+
+            if(count == 0){
+
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+
+                return true;
+
+            } else {
+
+                return false;
+
+            }
+
+        } else {
+
+            // Can't move if there's no direction to move in
+
+            return false;
+
+        }
+
+       
+
+    }
+
+ 
+
+    void OnMove(InputSystem movementValue) {
+
+        movementInput = movementValue.Get<Vector2>();
+
+    }
+}
