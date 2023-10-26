f4a1026011d088d0c2866a6a0330e2ddeec64708
Wu <s-wube@bsd405.org>
2023-10-26 13:25:52 -0700

    Added toolbar and can now switch from slot to slot in tool bar, working on drag and dropping items in inventory

--- Assets/Scripts/UI/Inventory_UI.cs
+++ Assets/Scripts/UI/Inventory_UI.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory_UI : MonoBehaviour
 {
     public GameObject inventoryPanel;
+
     public Player player;
 
     public List<Slot_UI> slots = new List<Slot_UI>();
+
+    [SerializeField] private Canvas canvas;
+
+    private Slot_UI draggedSlot;
+    private Image draggedIcon;
+
+    private void Awake() {
+        canvas = FindObjectOfType<Canvas>();
+    }
 
     void Update() {
         // Press TAB to open Inventory
@@ -41,5 +52,51 @@ public class Inventory_UI : MonoBehaviour
                 }
             }
         }
-    } 
+    }
+
+    /* public void Remove(int slotID) {
+        Item itemToDrop = GameManager.instance.itemManager.GetItemByName(
+            player.inventory.slots[slotID].itemName);
+
+        if(itemToDrop != null) {
+            player.DropItem(itemToDrop);
+            player.inventory.Remove(slotID);
+            Refresh();
+        }
+    } */
+
+    // Drag and dropping items
+    public void SlotBeginDrag(Slot_UI slot) {
+        draggedSlot = slot;
+        draggedIcon = Instantiate(draggedSlot.itemIcon);
+        draggedIcon.transform.SetParent(canvas.transform);
+        draggedIcon.raycastTarget = false;
+        draggedIcon.rectTransform.sizeDelta = new Vector2(50, 50);
+
+        MoveToMousePosition(draggedIcon.gameObject);
+        Debug.Log("Start Drag : " + draggedSlot.name);
+    }
+
+    public void SlotDrag() {
+        Debug.Log("Dragging: " + draggedSlot.name);
+    }
+
+    public void SlotEndDrag() {
+        Debug.Log("Done Dragging: " + draggedSlot.name);
+    }
+
+    public void SlotDrop(Slot_UI slot) {
+        Debug.Log("Dropped " + draggedSlot.name + " on " + slot.name);
+    }
+
+    private void MoveToMousePosition(GameObject toMove) {
+        if(canvas != null) {
+        Vector2 position;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform,
+            Input.mousePosition, null, out position);
+
+        toMove.transform.position = canvas.transform.TransformPoint(position);
+        }
+    }
 }
--- Assets/Scripts/UI/Slot_UI.cs
+++ Assets/Scripts/UI/Slot_UI.cs
@@ -9,6 +9,8 @@ public class Slot_UI : MonoBehaviour
     public Image itemIcon;
     public TextMeshProUGUI quantityText;
 
+    [SerializeField] private GameObject highlight;
+
     public void SetItem(Inventory.Slot slot) {
         if(slot != null) {
             itemIcon.sprite = slot.icon;
@@ -21,5 +23,9 @@ public class Slot_UI : MonoBehaviour
         itemIcon.sprite = null;
         itemIcon.color = new Color(1, 1, 1, 0);
         quantityText.text = "";
+    }
+
+    public void SetHighlight(bool isOn) {
+        highlight.SetActive(isOn);
     }
 }
