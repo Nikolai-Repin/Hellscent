9cf3929fed19fd2b7e46b382518ae190ddeec8c2
Wong <s-wongg@bsd405.org>
2023-10-27 13:09:19 -0700

    finished ui for keybinds

--- Assets/Scripts/Keybind.cs
+++ Assets/Scripts/Keybind.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using System;
 using UnityEngine;
 using TMPro;
@@ -7,29 +5,47 @@ using TMPro;
 public class Keybind : MonoBehaviour {
     
     [Header("Objects")]
-    [SerializeField] private TextMeshProUGUI buttonLbl;
+    [SerializeField] private TextMeshProUGUI buttonAttack;
+    [SerializeField] private TextMeshProUGUI buttonPickUp;
+    [SerializeField] private TextMeshProUGUI buttonItem;
     
     private void Start () {
-        buttonLbl.text = PlayerPrefs.GetString("CustomKey");
+        buttonAttack.text = PlayerPrefs.GetString("CustomKey1");
+        buttonPickUp.text = PlayerPrefs.GetString("CustomKey2");
+        buttonItem.text = PlayerPrefs.GetString("CustomKey3");
     }
 
     private void Update () {
+        ChangeKey(buttonAttack, "CustomKey1");
+        ChangeKey(buttonPickUp, "CustomKey2");
+        ChangeKey(buttonItem, "CustomKey3");
+        
+    }
 
-        if (buttonLbl.text == "Awaiting Input") {
+    public void ChangeAttackKey() {
+        buttonAttack.text = "Awaiting Input";
+    }
+
+    public void ChangePickUpKey() {
+        buttonPickUp.text = "Awaiting Input";
+    }
+
+    public void ChangeItemKey() {
+        buttonItem.text = "Awaiting Input";
+    }
+
+    public void ChangeKey(TextMeshProUGUI button, String k) {
+        if (button.text == "Awaiting Input") {
 
             foreach (KeyCode keycode in Enum.GetValues(typeof(KeyCode))) {
 
                 if (Input.GetKey(keycode)) {
 
-                    buttonLbl.text = keycode.ToString();
-                    PlayerPrefs.SetString("CustomKey", keycode.ToString());
+                    button.text = keycode.ToString();
+                    PlayerPrefs.SetString(k, keycode.ToString());
                     PlayerPrefs.Save();                
                 }
             }
         }
-    }
-
-    public void ChangeKey() {
-        buttonLbl.text = "Awaiting Input";
     }
 }
