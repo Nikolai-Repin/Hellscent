bb6240588f80c942ee433d18a1f11ac012d0f015
Repin <s-repinn@bsd405.org>
2023-10-30 13:07:28 -0700

    created basic enemy spawning

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,6 +6,15 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private float radius;
     [SerializeField] private List<GameObject> enemyPool;
+    [SerializeField] private int enemyCount;
+
+    void Start() {
+        for (int i = 0; i < enemyCount; i++) {
+            float dist = Random.value * radius;
+            Vector2 position = new Vector2(transform.position.x + (Mathf.Cos(Random.Range(0, 360) * Mathf.Deg2Rad) * dist), transform.position.y + (Mathf.Sin(Random.Range(0, 360) * Mathf.Deg2Rad) * dist));
+            GameObject enemy = Instantiate(enemyPool[Random.Range(0, enemyPool.Count - 1)], position, Quaternion.Euler(0, 0, 0));
+        }
+    }
 
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.green;
