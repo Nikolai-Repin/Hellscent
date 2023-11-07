49885c84375b1f9d2a321a3d13b728a13215075f
Minascurta <s-minascurtaa@bsd405.org>
2023-11-07 13:09:10 -0800

    Added new sprites for testing, added new script to access the jornal

---
+++ Assets/Scripts/Journal navigation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Journalnavigation : MonoBehaviour
+{
+    private bool journalOpen = false;
+    private bool isOpen = false;
+
+    [SerializeField] private GameObject[] pages;
+    GameObject book;
+    private int page = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J)){
+            journalOpen = !journalOpen;
+            
+        }
+        if(journalOpen && !isOpen){
+            isOpen = !isOpen;
+            book = Instantiate(pages[page], new Vector2(0, 0), transform.rotation);
+        }
+    }
+}
