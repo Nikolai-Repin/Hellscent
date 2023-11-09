32c30c6a97570ead686d6a055da864b9871b9982
Wong <s-wongg@bsd405.org>
2023-11-09 12:58:02 -0800

    keybinds work

--- Assets/Scripts/Keybind.cs
+++ Assets/Scripts/Keybind.cs
@@ -6,8 +6,8 @@ public class Keybind : MonoBehaviour {
     
     [Header("Objects")]
     [SerializeField] private TextMeshProUGUI buttonAttack;
-    [SerializeField] private TextMeshProUGUI buttonPickUp;
-    [SerializeField] private TextMeshProUGUI buttonItem;
+    [SerializeField] private TextMeshProUGUI buttonGrab;
+    [SerializeField] private TextMeshProUGUI buttonSwap;
     
     private void Start () {
         if (PlayerPrefs.GetInt("Attack") == 0) {
@@ -18,23 +18,26 @@ public class Keybind : MonoBehaviour {
 
             buttonAttack.text = PlayerPrefs.GetString("aText");
         } if (PlayerPrefs.GetInt("Grab") == 0) {
-            PlayerPrefs.SetInt("Grab", (int) KeyCode.E);
-            buttonPickUp.text = "E";
-        } else {
-            buttonPickUp.text = PlayerPrefs.GetString("gText");
 
-        } if (PlayerPrefs.GetInt("Swap") == 0) {
-            PlayerPrefs.SetInt("Swap", (int) KeyCode.R);
-            buttonItem.text = "R";
+            PlayerPrefs.SetInt("Grab", (int) KeyCode.E);
+            buttonGrab.text = "E";
         } else {
-            buttonItem.text = PlayerPrefs.GetString("sText");
+
+            buttonGrab.text = PlayerPrefs.GetString("gText");
+        } if (PlayerPrefs.GetInt("Swap") == 0) {
+            
+            PlayerPrefs.SetInt("Swap", (int) KeyCode.R);
+            buttonSwap.text = "R";
+        } else {
+
+            buttonSwap.text = PlayerPrefs.GetString("sText");
         }
     }
 
     private void Update () {
         ChangeKey(buttonAttack, "Attack", "aText");
-        ChangeKey(buttonItem, "Grab", "gText");
-        ChangeKey(buttonPickUp, "Swap", "sText");
+        ChangeKey(buttonSwap, "Swap", "sText");
+        ChangeKey(buttonGrab, "Grab", "gText");
         
     }
 
@@ -42,12 +45,12 @@ public class Keybind : MonoBehaviour {
         buttonAttack.text = "Awaiting Input";
     }
 
-    public void ChangePickUpKey() {
-        buttonPickUp.text = "Awaiting Input";
+    public void ChangeGrabKey() {
+        buttonGrab.text = "Awaiting Input";
     }
 
-    public void ChangeItemKey() {
-        buttonItem.text = "Awaiting Input";
+    public void ChangeSwapKey() {
+        buttonSwap.text = "Awaiting Input";
     }
 
     public void ChangeKey(TextMeshProUGUI button, String k, String t) {
