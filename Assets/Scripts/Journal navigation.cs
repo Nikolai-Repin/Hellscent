5cbdc3a8ff4d4363946dd94e97fba05ad0579e9b
Minascurta <s-minascurtaa@bsd405.org>
2023-11-16 13:06:56 -0800

    Reworking the page navigation to make it work

--- Assets/Scripts/Journal navigation.cs
+++ Assets/Scripts/Journal navigation.cs
@@ -9,8 +9,13 @@ public class Journalnavigation : MonoBehaviour
 
     [SerializeField] private GameObject[] pages;
     [SerializeField] private GameObject[] texts;
+
     GameObject book;
+    GameObject textLeft;
+    GameObject textRight;
+
     private int page = 0;
+    //half the value of the length of texts due to every increase will display 2 pages at once
     private int text = 0;
 
     // Start is called before the first frame update
@@ -25,21 +30,46 @@ public class Journalnavigation : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.J)){
             journalOpen = !journalOpen;
             OpenBook();
+            Debug.Log("book is opened");
         }
 
         if(isOpen){
-            if(Input.GetKeyDown(KeyCode.RightArrow)){          
-                if(page < pages.Length - 1){
+            
+
+            if(Input.GetKeyDown(KeyCode.RightArrow)){ 
+                Debug.Log("page:" + page + "\ntexts " + text + ", " + (text + 1));
+                if(page == 1 && text < texts.Length / 2){
+                    ChangeText();
+                    text = text + 2;
+                }
+
+
+                else if(page < pages.Length - 1){
                     page++;
                     ChangePage();
+                    ChangeText();
+                    if(page == pages.Length - 1){
+                        RemoveText();
+                    }
                 }
+
+                
             }
-            else if(Input.GetKeyDown(KeyCode.LeftArrow)){
-                if(page > 0){
-                    page--;
-                    ChangePage();
-                }
-            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 
@@ -61,7 +91,50 @@ public class Journalnavigation : MonoBehaviour
     }
 
     private void ChangeText(){
-        
+        Destroy(textLeft);
+        Destroy(textRight);
+        textLeft = Instantiate(texts[text], new Vector2(0, 0), transform.rotation);
+        textRight = Instantiate(texts[text + 1], new Vector2(0, 0), transform.rotation);
+    }
+
+    private void RemoveText(){
+        Destroy(textLeft);
+        Destroy(textRight);
     }
 
 }
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+if(Input.GetKeyDown(KeyCode.RightArrow)){    
+                if(page == 1 && text < texts.Length - 1){
+                    ChangeText();
+                    text = text+2;
+                }      
+                else if(page < pages.Length - 1){
+                    page++;
+                    ChangePage();
+                    RemoveText();
+                }
+            }
+            else if(Input.GetKeyDown(KeyCode.LeftArrow)){
+                if(page == 1 && text > 1){
+                    text = text - 2;
+                    ChangeText();
+                }   
+                else if(page > 0){
+                    page--;
+                    ChangePage();
+                    RemoveText();
+                }
+            }*/
